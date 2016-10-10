0c689dfd79e54739a631dbd1a201260c1a87ed97
Albert <awhite2017@chadwickschool.org>
2016-10-09 22:03:59 -0700

    added first square line turn command and def.

--- Square.py
+++ Square.py
@@ -1,4 +1,14 @@
 #first line
 from Myro import *
- init("sim") #only if sim is not open
-
+init("sim") #only if sim is not open
+def move1(): #move the robot forward then right
+    penDown()
+    forward(4,1)
+    penUp()
+    turnBy(15)
+    turnBy(15)
+    turnBy(15)
+    turnBy(15)
+    turnBy(15)
+    turnBy(15)
+move1()
