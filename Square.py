98d02b681f7d13b0fde1f83005a40545edc43550
awhite2017 <awhite2017@chadwickschool.org>
2016-10-11 08:53:39 -0700

    make functions for shapes

--- Square.py
+++ Square.py
@@ -1,73 +1,52 @@
 #first line
 from Myro import *
 init("sim") #only if sim is not open
-def movesquare(): #move for the square
+
     turnBy(180)
     forward(1,1)
     turnBy(90)
     forward(2,1)
     turnBy(90)
+def movesquare(): #move for the square    
     penDown()
     forward(4,1)
     forward(4,.6)
     penUp()
     turnBy(90)#move the second part of the square
-    penDown()
-    forward(4,1)
-    forward(4,.6)
-    penUp()
-    turnBy(90) #move the third part of the square
-    penDown()
-    forward(4,1)
-    forward(4,.6)
-    penUp()
-    turnBy(90) #last move for the square
-    penDown()
-    forward(4,1)
-    forward(4,.6)
-    penUp()
-    turnBy(90) #square complete
-movesquare()
-def pentagon(): #writes a hexagon 
-    forward(.5,1)
-    turnBy(90)
-    forward(3.5,1)
-    turnBy(308)
+    
+
+   
+def pentagon(): #writes a hexagon  
     penDown()
     forward(3,1)
     penUp()
     turnBy(288)
-    penDown()
-    forward(3,1)
-    penUp()
-    turnBy(288)
-    penDown()
-    forward(3,1)
-    penUp()
-    turnBy(288)
-    penDown()
-    forward(3,1)
-    penUp()
-    turnBy(288)
-    penDown()
-    forward(3,1)
-pentagon()
+    
 def triangle(): #create triangle
     penUp()
     turnBy(253)
     forward(1,1)
     penDown()
     forward(2,1)
-    penUp()
-    turnBy(240)
-    penDown()
-    forward(2,1)
-    penUp()
-    turnBy(240)
-    penDown()
-    forward(2,1)
-    penUp()
+    
+movesquare() #moves square
+movesquare()
+movesquare()
+movesquare()
+
+forward(.5,1) #get ready for pentagon
+turnBy(90)
+forward(3.5,1)
+turnBy(308)
+
+pentagon() #move pentagon
+pentagon()
+pentagon()
+pentagon()
+penDown()
+forward(3,1)
 triangle()
+penUp()
 
     
 
